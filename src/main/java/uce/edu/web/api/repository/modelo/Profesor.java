b1ded7b7f178f6da95af1d4ffeffee4cc4f53e56
JSquinatoa <jsquinatoa@uce.edu.ec>
2025-06-22 10:49:32 -0500

    Tarea 7: HTTP Request - Response Profesor

---
+++ src/main/java/uce/edu/web/api/repository/modelo/Profesor.java
@@ -0,0 +1,69 @@
+package uce.edu.web.api.repository.modelo;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "profesor")
+public class Profesor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "prof_id")
+    private Integer id;
+    @Column(name = "prof_nombre")
+    private String nombre;
+    @Column(name = "prof_apellido")
+    private String apellido;
+    @Column(name = "prof_fechaNacmimiento")
+    private LocalDateTime fechaNacmimiento;
+    @Column(name = "prof_sueldo")
+    private Integer sueldo;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public LocalDateTime getFechaNacmimiento() {
+        return fechaNacmimiento;
+    }
+
+    public void setFechaNacmimiento(LocalDateTime fechaNacmimiento) {
+        this.fechaNacmimiento = fechaNacmimiento;
+    }
+
+    public Integer getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(Integer sueldo) {
+        this.sueldo = sueldo;
+    }
+
+}
